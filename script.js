830f93e731389f392e3eea54b55a4ec2473c04ab
Yana Trots <jana.trots@digitalizm.com>
2022-07-16 22:19:45 +0300

    add Javascript solution

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function ClickFunc() {
+    document.getElementsByClassName("social")[0].classList.add("social-withjs");
+    document.getElementsByClassName("share-btn")[0].classList.add("share-btn-withjs");
+};
 
+function MouseLeaveFunc(){
+    document.getElementsByClassName("social")[0].classList.remove("social-withjs");
+    document.getElementsByClassName("share-btn")[0].classList.remove("share-btn-withjs");
+}
