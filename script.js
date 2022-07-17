f477ce15b8f9b185545cea5b891b1e258650411c
Yana Trots <jana.trots@digitalizm.com>
2022-07-17 15:44:09 +0300

    add js for small screen on index-alt page

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function ClickFunc() {
 
     let foomobile = document.getElementsByClassName("footnote-mobile")[0];
-    let sharebtn = document.getElementsByClassName("share-btn")[0];
     let footnote = document.getElementsByClassName("footnote")[0];
+    let sharebtn = document.getElementsByClassName("share-btn")[0];
     let social = document.getElementsByClassName("social")[0];
+    let sharebtn_active = document.getElementsByClassName("footnote-mobile")[0].getElementsByClassName("share-btn")[0];
 
     social.classList.add("social-withjs");
     sharebtn.classList.add("share-btn-withjs");
@@ -13,7 +14,7 @@ function ClickFunc() {
         foomobile.style.display = "block";
 
         document.addEventListener("click", function(e){
-            if (foomobile.contains(e.target)){
+            if (sharebtn_active.contains(e.target)){
                 foomobile.style.display = "none";
                 footnote.style.display = "flex";    
             }
